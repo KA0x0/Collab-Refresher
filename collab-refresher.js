84acf019bfac65e88925ec733fc0e18694083ac5
KH405-TV <jasonmercier666@gmail.com>
2021-05-19 15:11:39 -0400

    Update collab-refresher.js

--- collab-refresher.js
+++ collab-refresher.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name        Collab Refresher
-// @namespace   https://codeberg.org/KHAOZ
+// @namespace   https://github.com/KH405-TV
 // @author      KH405
-// @license     MIT
+// @license     GPL-3
 // @version     1.0.0
 // @description Refresh Google Collab so it doesn't time out
 // @grant       None
-// @homepageURL https://codeberg.org/KHAOZ/Collab-Refresher/
-// @supportURL  https://codeberg.org/KHAOZ/Collab-Refresher/-/issues
-// @downloadURL https://codeberg.org/KHAOZ/Collab-Refresher/-/raw/master/collab-refresher.js
-// @updateURL   https://codeberg.org/KHAOZ/Collab-Refresher/-/raw/master/collab-refresher.js
+// @homepageURL https://github.com/KH405-TV/Collab-Refresher
+// @supportURL  https://github.com/KH405-TV/Collab-Refresher/issues
+// @downloadURL https://github.com/KH405-TV/Collab-Refresher/blob/master/collab-refresher.js
+// @updateURL   https://github.com/KH405-TV/Collab-Refresher/blob/master/collab-refresher.js
 // ==/UserScript==
 function ClickConnect(){
 document.querySelector("colab-toolbar-button#connect").click() 
